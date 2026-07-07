4cd37c9e6b708d882bf864d397a58e5af6c867f3
raghav <mangaliraghav1111@gmail.com>
2026-07-07 14:50:08 +0530

    used conditinal statements

--- file2.java
+++ file2.java
@@ -4,6 +4,10 @@
         
         String s="10";
         String s2="10";
-        System.err.println(s==s2);
+         if (s == s2) {
+         System.out.print("Both  have same Refernce Both located at same");
+      } else {
+         System.out.print("Both  have diff Refernce ");
+      }
     }
 }
